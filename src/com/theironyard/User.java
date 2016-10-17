aafd8f5a5bd3e283bf2c6e5c5bbf93309a572648
oakes <zsoakes@gmail.com>
2016-10-17 12:06:05 -0400

    Something ain't right

---
+++ src/com/theironyard/User.java
@@ -0,0 +1,14 @@
+package com.theironyard;
+
+/**
+ * Created by zach on 10/17/16.
+ */
+public class User {
+    int id;
+    String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
